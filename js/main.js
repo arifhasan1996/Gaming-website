25b88c240540c7872862a0538ba1d63dab22482f
Arif <Arif>
2023-11-08 18:37:08 +0600

    Mobile and desktop version is done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// let platformContainer = document.getElementById('platform-container');
+// let navLink = document.querySelectorAll('nav-link');
+
+// platformContainer.style.display = 'none';
+
+// function onMouseOver(){
+//     if(platformContainer.style.display == 'none'){
+//         platformContainer.style.display = 'block';
+//     }
+// }
